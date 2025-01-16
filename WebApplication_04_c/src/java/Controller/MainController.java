e61018dbf7730d6dab812deb052d7d09780925b1
DevMinh <devminhprogram@gmail.com>
2025-01-16 14:33:07 +0700

    ye

---
+++ WebApplication_04_c/src/java/Controller/MainController.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ducmi
+ */
+@WebServlet(name = "MainController", urlPatterns = {"/MainController"})
+public class MainController extends HttpServlet {
+    public boolean isValidLogin(String username, String password){
+        return username.equals("admin") && password.equals("12345678");
+    }
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String username = request.getParameter("txtUsername");
+        String password = request.getParameter("txtPassword");
+
+    if (username == null || username.trim().length() == 0) {
+        out.println("Please enter Username!");
+        return;
+    }
+    
+    if (password == null || password.trim().length() < 8){
+        out.println("Password must be atleast 8 ditgit!");
+        return;
+    }
+    
+    //Login Process
+//    if (username.equals("admin") && password.equals("12345678")) {
+//        out.println("Login successful!");
+//    } else {
+//        out.println("Login failed! Incorrect username or password.");
+//    }
+    
+    if(isValidLogin(username, password)){
+            RequestDispatcher rd = request.getRequestDispatcher("Search.html");
+            rd.forward(request, response);
+        }else{
+           response.sendRedirect("Invalid.html");
+    }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
